cf647373cd7426063702cb7eaf02de243eb863f0
forestip <forestip@127.0.0.1>
2015-01-23 15:33:51 +0100

    Triggers de remise unique fonctionnels (testés)

--- VePick/triggers/remise_noncumulable.sql
+++ VePick/triggers/remise_noncumulable.sql
@@ -2,17 +2,20 @@ CREATE OR REPLACE TRIGGER REMISE_UNIQUE
   BEFORE INSERT ON RemiseNonAbonne
   FOR EACH ROW
 DECLARE
-  remise int;
+  nbRemise int;
 BEGIN
-  SELECT COUNT(numRemise) INTO remise
-  FROM NonAbonne
-  WHERE numRemise =:new.numRemise;
+  SELECT COUNT(numRemise) INTO nbRemise
+  FROM RemiseNonAbonne
+  WHERE numClient =:new.numClient;
     
-  IF (remise <> 0)
-  THEN raise_application_error(-20000 , 'Vous avez deja une remise');
+  IF (nbRemise <> 0)
+  THEN raise_application_error(-20000 , 'Vous avez deja une remise ');
   END IF;
 END;
 /
 
 /*Lorsqu'on conssome une remise, on passe la reference numremise de client a null et on supprime le tuple remise correspondant */
-
+/*
+On considere que le client non abonne 1 a deja la remiseNonAbo 1
+INSERT INTO RemiseNonAbonne values (2, '0000001', 10, TO_DATE('2015/02/01 16:00:00', 'yyyy/mm/dd hh24:mi:ss'),1);
+*/
--- VePick/triggers/remise_noncumulable_abonne.sql
+++ VePick/triggers/remise_noncumulable_abonne.sql
@@ -2,16 +2,19 @@ CREATE OR REPLACE TRIGGER REMISE_UNIQUE_ABONNE
   BEFORE INSERT ON RemiseAbonne
   FOR EACH ROW
 DECLARE
-  remise int;
+  nbRemise int;
 BEGIN
-  SELECT COUNT(numRemise) INTO remise
-  FROM Abonne
-  WHERE numRemise =:new.numRemise;
+  SELECT COUNT(numRemise) INTO nbRemise
+  FROM RemiseAbonne
+  WHERE numClient =:new.numClient;
     
-  IF (remise <> 0)
-  THEN raise_application_error(-20000 , 'Vous avez deja une remise');
+  IF (nbRemise <> 0)
+  THEN raise_application_error(-20000 , 'Vous avez deja une remise abonne');
   END IF;
 END;
 /
 
 /*Lorsqu'on conssome une remise, on passe la reference numremise de client a null et on supprime le tuple remise correspondant */
+/*On considere que l'abonne 2 a deja la remiseAbo 1
+INSERT INTO RemiseAbonne values (2,10, 2);
+*/
