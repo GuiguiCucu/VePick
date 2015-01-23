d94d1ae69d8f16f824e0a039be833b18da1a602c
cutroneg <guillaume.cutrone@gmail.com>
2015-01-23 11:58:01 +0100

    trigger rendu_velo

--- VePick/triggers/rendu_velo.sql
+++ VePick/triggers/rendu_velo.sql
@@ -1,22 +1,23 @@
 CREATE OR REPLACE TRIGGER RENDU_VELO
 	BEFORE UPDATE ON Bornette
 	FOR EACH ROW
+DECLARE
+	oldNumVelo int;
+	newNumVelo int;
+	etat varchar(2);
 BEGIN
-	-- si on veut rendre un vélo
-	if (:old.numvelo <> :new.numVelo) then
+	oldNumVelo:=:old.numVelo;
+	newNumVelo:=:new.numVelo;
+	etat:=:old.etat;
 	
-		raise_application_error(-20001 , 'fuuf');
-		
-		-- si la borne est déjà occupée
-		if(:old.numVelo <> NULL)
-			then raise_application_error(-20001 , 'La borne est déjà occupée');
-		end if;
-		
-		-- si la borne est HS
-		if (:old.etat = 'HS')
-			then raise_application_error(-20002 , 'La borne est HS');
-		end if;
-		
+	-- si on veut rendre un vélo sur une borne deja occupee
+	if (oldNumVelo IS NOT NULL AND oldNumVelo <> newNumVelo) then
+		raise_application_error(-20001 , 'La borne est deja occupee');
+	end if;
+	
+	-- si on veut rendre un vélo sur une borne HS
+	if (oldNumVelo IS NULL AND newNumVelo IS NOT NULL AND etat = 'HS') then
+		raise_application_error(-20002 , 'La borne est HS');
 	end if;
 END;
 /
@@ -27,8 +28,8 @@ END;
 -- on essaie de rendre un vélo sur une borne occupée 
 
 UPDATE Bornette
-SET numVelo = 2
-WHERE numBornette = 1;
+SET numVelo = 1
+WHERE numBornette = 9;
 
 -- sur une borne HS
 
