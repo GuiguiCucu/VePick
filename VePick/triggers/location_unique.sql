c3a03f6e46b1ce870b2a5d0c4283625114222845
forestip <forestip@127.0.0.1>
2015-01-27 17:13:12 +0100

    MAJ trigger de location unique

--- VePick/triggers/location_unique.sql
+++ VePick/triggers/location_unique.sql
@@ -6,11 +6,10 @@ DECLARE
 BEGIN
   SELECT COUNT(numClient) INTO loc
   FROM Location
-  WHERE numClient =:new.numClient
-  AND dateFinLocation IS NULL;
+  WHERE numClient =:new.numClient;
     
   IF (loc <> 0)
-  THEN raise_application_error(-20000 , 'Impossible de louer deux vélos en même temps!');
+  THEN raise_application_error(-20000 , 'Impossible de louer deux vlos en mme temps!');
   END IF;
   
 END;
@@ -19,7 +18,7 @@ END;
 /*
 ===== TESTS =====
 
--- on sait que le Vélo 1 est en cours de location par Client 1 mais toujours pas rendu, on tente une location sur un autre vélo par ce client.
+-- on sait que le Vlo 1 est en cours de location par Client 1 mais toujours pas rendu, on tente une location sur un autre vlo par ce client.
 
 INSERT INTO Location values (1, 3, TO_DATE('2015/01/16 14:34:12', 'yyyy/mm/dd hh24:mi:ss'),NULL, 'Vplus', 'Vmoins');
 
