2e8b9952b3c2e802c2829e3564f493ee2992efe0
forestip <forestip@127.0.0.1>
2015-01-27 15:24:09 +0100

    MAJ insert

--- VePick/population_bd/insert.sql
+++ VePick/population_bd/insert.sql
@@ -79,7 +79,7 @@ INSERT INTO RemiseNonAbonne values (1, '0000001', 10, TO_DATE('2015/02/01 16:00:
 INSERT INTO RemiseAbonne values (1,10, 2);
 
 
-INSERT INTO Location values (1, 1, TO_DATE('2015/01/16 14:34:11', 'yyyy/mm/dd hh24:mi:ss'),NULL, 'Vplus', 'Vmoins');
+INSERT INTO Location values (1, 1, TO_DATE('2015/01/16 14:34:11', 'yyyy/mm/dd hh24:mi:ss'),NULL, 'Vplus', NULL);
 INSERT INTO Location values (2, 3, TO_DATE('2015/01/12 12:20:35', 'yyyy/mm/dd hh24:mi:ss'), TO_DATE('2015/01/12 13:05:11', 'yyyy/mm/dd hh24:mi:ss'), 'Vnul','Vnul');
 
 INSERT INTO Location values (4, 4, TO_DATE('2015/01/10 14:34:00', 'yyyy/mm/dd hh24:mi:ss'),NULL, 'Vplus', 'Vmoins');
