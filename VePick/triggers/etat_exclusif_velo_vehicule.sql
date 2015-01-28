290c9b6cbcc54b5645744b27cd50b631c56a3206
lafonth <lafonth@127.0.0.1>
2015-01-28 11:00:00 +0100

    DERNIER PUTAIN DE TRIGGER DES ETATS DU VELO !!!!

--- VePick/triggers/etat_exclusif_velo_vehicule.sql
+++ VePick/triggers/etat_exclusif_velo_vehicule.sql
@@ -4,27 +4,30 @@ CREATE OR REPLACE TRIGGER ETAT_EXCLUSIF_VELO_VEHICULE
 DECLARE
   dateDernierChargement date;
   dateDernierDepot date;
-  surBornetter int;
+  surBornette int;
+  estLoue int;
 BEGIN
-  
-  SELECT COUNT(numVelo) INTO surBornette
-  FROM Bornette
-  WHERE numVelo = :new.numVelo
-  
-  IF( surBornette <> 0)
-  THEN UPDATE Bornette SET numVelo = null WHERE numVelo = :new.numVelo
-  END IF;
-  
-  SELECT COUNT(numClient) INTO estLoue
-  FROM Location
-  WHERE dateFinLocation IS NULL
-  AND numVelo = :new.numVelo;
-  
-  IF(estLoue <> 0)
-  THEN 
-  	--TODO
-  END IF;
-  
+   
+	IF(:new.nomAction =  'Chargement velo') THEN	
+		  SELECT COUNT(numVelo) INTO surBornette
+		  FROM Bornette
+		  WHERE numVelo = :new.numVelo;
+		  
+		  IF( surBornette <> 0)
+		  		THEN UPDATE Bornette SET numVelo = null WHERE numVelo = :new.numVelo;
+		  END IF;
+		  
+		  
+		  SELECT COUNT(numClient) INTO estLoue
+		  FROM Location
+		  WHERE dateFinLocation IS NULL
+		  AND numVelo = :new.numVelo;
+		  
+		  IF(estLoue <> 0) THEN
+		  		raise_application_error(-20000 , 'Impossible de charger le vélo il est en cours de location, il doit d abord etre remis à une bornette par le client.');
+		  END IF;
+	 
+	END IF;
  
 END;
 /
@@ -32,10 +35,10 @@ END;
 /*
 ===== TESTS =====
 
--- On sait que vélo X est accroché à une bornette, on tente de charger le vélo, verifier que le vélo est b
-(TODO DEMANDER LES CONDITION OU CA PEUT MARCHER)ien decroché de cette bornette
---TODO
+-- On sait que vélo 7 est accroché à la bornette 7, on tente de charger le vélo, verifier que le vélo est bien decroché de cette bornette
+-- INSERT INTO ActionVehicule values (3, SYSDATE);
+-- INSERT INTO ActionVehiculeVelo values (3, 3, 7, 'Chargement velo');
 
---On sait que le Vélo X est en location, on tente de charger un vélo loué, verifier qu'une erreur est retourné (EXTENSION: on peut autorisé le cassi c'est un vélo abandonné)
---TODO
+--On sait que le Vélo 1 est en location, on tente de le charger, verifier qu'une erreur est retourné (EXTENSION: on peut autorisé le cas si c'est un vélo abandonné)
+-- INSERT INTO ActionVehiculeVelo values (3, 3, 1, 'Chargement velo');
 */
