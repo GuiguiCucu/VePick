5c4ab60a79989e02a9a2fe6f1e48980a13e2fd57
neveucl <neveucl@127.0.0.1>
2015-01-28 11:29:26 +0100

    jeej

--- VePick/triggers/ordre_routine.sql
+++ VePick/triggers/ordre_routine.sql
@@ -1,19 +1,31 @@
--- idée du truc: faire une seule grosse requête (sans "for each row") pour voir s'il n'existe pas de TacheRoutine de rang > qui serait validée (jeeeeeeeej)
-
 CREATE OR REPLACE TRIGGER ORDRE_ROUTINE
-	BEFORE UPDATE ON TacheRoutine
+	AFTER UPDATE ON TacheRoutine
 DECLARE
-	prochaineTache int;
+	nb int;
 BEGIN
-	-- On enregistre la prochaine tâche, c'est à dire le rang le plus petit qui est "en attente"
-	SELECT MIN(rang) INTO prochaineTache
-	FROM TacheRoutine
-	WHERE numVehicule=:old.numVehicule AND etat='En attente';
-
-  	-- si on essaye d'en modifier un autre --> erreur
-	if (:old.rang <> prochaineTache) then
-		raise_application_error(-20005 , 'Il ya a des taches précédentes encore en attente');
+	-- On recherche pour chaque véhicule s'il n'a pas de tâche validé de rang > à la prochaine
+FOR LIGNE IN (SELECT DISTINCT T.numVehicule, T.numTache, MIN(rang) AS minimum 
+		FROM TacheRoutine T, VehiculeRegulation V WHERE T.numVehicule = V.numVehicule 
+		AND T.etat='En attente' GROUP BY T.numVehicule, T.numTache) LOOP
+	--dbms_output.put_line('Début avec : idRoutines '|| LIGNE.idRoutines || 'et minimum : ' || LIGNE.minimum);	
+	SELECT count(numTache) INTO nb
+	FROM TacheRoutine 
+	WHERE numVehicule = LIGNE.numVehicule AND numTache = LIGNE.numTache
+	AND rang > LIGNE.minimum
+	AND numVehicule IN (SELECT numVehicule
+						FROM TacheRoutine
+						WHERE etat <> 'En attente')
+	AND numTache IN (SELECT numTache
+						FROM TacheRoutine
+						WHERE etat <> 'En attente')
+	AND rang IN (SELECT rang
+						FROM TacheRoutine
+						WHERE etat <> 'En attente');
+	--dbms_output.put_line('idRoutines '|| LIGNE.idRoutines || 'avec' || nb);	
+	if(nb > 0) then
+		Raise_application_error(-20013,'Vous devez valider toutes les taches précédentes');
 	end if;
+END LOOP ;
 	
 END;
 /
@@ -37,10 +49,22 @@ WHERE numVehicule=1 AND numTache=1 AND rang=1;
 
 Valider une tache qui n'est pas la prochaine:
 UPDATE TacheRoutine 
-SET etat="Echec"
-WHERE numVehicule=1 AND numTache=1 AND rang=1;
+SET etat='Echec'
+WHERE numVehicule=1 AND numTache=1 AND rang=3;
 
-
+SELECT count(numTache)
+	FROM TacheRoutine 
+	WHERE numVehicule = 1 AND numTache = 1 
+	AND rang > 2
+	AND numVehicule IN (SELECT numVehicule
+						FROM TacheRoutine
+						WHERE etat <> 'En attente')
+	AND numTache IN (SELECT numTache
+						FROM TacheRoutine
+						WHERE etat <> 'En attente')
+	AND rang IN (SELECT rang
+						FROM TacheRoutine
+						WHERE etat <> 'En attente');
 
 SELECT MIN(rang) FROM TacheRoutine WHERE numVehicule=1 AND etat='En attente';
 */
