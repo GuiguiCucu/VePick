14df148acfe4ca7bf85eea0f19a72d215145bd11
lafonth <lafonth@127.0.0.1>
2015-01-28 11:07:15 +0100

    maj import_trigger

--- VePick/triggers/import_trigger.sql
+++ VePick/triggers/import_trigger.sql
@@ -4,3 +4,6 @@ start location_unique.sql
 start rendu_velo.sql
 start remise_noncumulable.sql
 start remise_noncumulable_abonne.sql
+start etat_exclusif_velo_location.sql
+start etat_exclusif_velo_vehicule.sql
+start etat_exclusif_velo_bornette.sql
